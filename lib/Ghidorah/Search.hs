cadb7230acc68412fbab4339b3391545496f3a16
Marco Benelli <mbenelli@fastmail.com>
2024-02-08 17:58:56 +0100

    Add server

--- lib/Ghidorah/Search.hs
+++ lib/Ghidorah/Search.hs
@@ -17,25 +17,97 @@ data IncludedFields = IncludedFields
 instance FromJSON IncludedFields
 
 
---data Field = Field
---  { self :: !Text
---  , value :: !Text
---  --, id :: !Text
---  , disabled :: !Bool
---  } deriving (Show, Generic)
+data ChangeDetails = ChangeDetails
+  { field :: !String
+  , fieldId :: !String
+  , fieldtype :: !String
+  , from :: !String
+  , fromString :: !String
+  , to :: !String
+  , toString :: !String
+  } deriving (Show, Generic)
 
+instance FromJSON ChangeDetails
+
+data HistoryMetadataPartecipant = HistoryMetadataPartecipant
+  { avatarUrl :: !(Maybe Text)
+  , displayName :: !(Maybe Text)
+  , displayNameKey :: !(Maybe Text)
+  , id''' :: !(Maybe Text)
+  , type'' :: !(Maybe Text)
+  , url' :: !(Maybe Text)
+  } deriving (Show, Generic)
+
+instance FromJSON HistoryMetadataPartecipant
+
+data HistoryMetadata = HistoryMetadata
+  { activityDescription :: !(Maybe Text)
+  , activityDescriptionKey :: !(Maybe Text)
+  , actor :: !(Maybe HistoryMetadataPartecipant)
+  , cause :: !(Maybe HistoryMetadataPartecipant)
+  , description :: !(Maybe Text)
+  , descriptionKey :: !(Maybe Text)
+  , emailDescription :: !(Maybe Text)
+  , emailDescriptionKey :: !(Maybe Text)
+  , generator :: !(Maybe HistoryMetadataPartecipant)
+  } deriving (Show, Generic)
+
+instance FromJSON HistoryMetadata
+
+data AvatarUrlsBean = AvatarUrlsBean
+  { avatar16x16 :: !(Maybe Text)
+  , avatar24x24 :: !(Maybe Text)
+  , avatar32x32 :: !(Maybe Text)
+  , avatar48x48 :: !(Maybe Text)
+  } deriving (Show, Generic)
+
+instance FromJSON AvatarUrlsBean
+
+data UserDetail = UserDetail
+  { accountId :: !(Maybe Text)
+  , accountType :: !(Maybe Text)
+  , active :: !(Maybe Bool)
+  , avatarUrls :: !(Maybe AvatarUrlsBean)
+  , displayName' :: !(Maybe Text)
+  , emailAddress :: !(Maybe Text)
+  , key :: !(Maybe Text)
+  , name :: !(Maybe Text)
+  , self :: !(Maybe Text)
+  , timeZone :: !(Maybe Text)
+  } deriving (Show, Generic)
+
+instance FromJSON UserDetail
+
+data Changelog = Changelog
+  { author :: !(Maybe UserDetail)
+  , created :: !Text
+  , historyMetadata :: !(Maybe HistoryMetadata)
+  , id'' :: !(Maybe Text)
+  , items :: !(Maybe [ChangeDetails])
+  } deriving (Show, Generic)
+
+instance FromJSON Changelog
+
+data PageOfChangelogs = PageOfChangelogs
+  { histories :: !(Maybe [Changelog])
+  , maxResults' :: !(Maybe Int)
+  , startAt' :: !(Maybe Int)
+  , total' :: !(Maybe Int)
+  } deriving (Show, Generic)
+
+instance FromJSON PageOfChangelogs
 
 
 data IssueBean = IssueBean
   {
-  -- changelog :: !Text -- PageOfChangelogs
+  changelog' :: !(Maybe PageOfChangelogs)
   --, editmeta :: !Text -- IssueUpdateMetadata
-  --, expand_ :: !Text
-  fields :: !(Maybe [Field])
+  , expand' :: !(Maybe Text)
+  , fields :: !(Maybe [Field])
   , fieldsToInclude :: !(Maybe IncludedFields)
   --,
   , id :: !Text
-  , key :: !Text
+  , key' :: !Text
   --, names_ :: !Text -- JSON
   --, operation :: !Text -- Operations
   --, properties :: !Text -- JSON
---
+++ lib/Ghidorah/Server.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Ghidorah.Server where
+
+import Data.Aeson
+import Data.Text 
+import GHC.Generics
+import Network.Wai.Handler.Warp
+import Servant
+
+import Ghidorah.Search (Changelog, IssueBean, UserDetail)
+
+
+data IssueRequest = IssueRequest
+  { timestamp :: !(Maybe Text)
+  , webhookEvent :: !(Maybe Text)
+  , issue_event_type_name :: !(Maybe Text)
+  , user :: !(Maybe UserDetail)
+  , issue :: !(Maybe IssueBean)
+  , changelog :: !(Maybe Changelog)
+} deriving (Show, Generic)
+
+instance FromJSON IssueRequest
+
+type API = "issue" :> ReqBody '[JSON] IssueRequest :> Post '[PlainText] Text
+
+processRequest :: IssueRequest -> Text
+processRequest x = pack $ show x
+
+api :: Proxy API
+api = Proxy
+
+server :: Server API
+server = return . processRequest
+
+app :: Application
+app = serve api server
+
+run :: IO ()
+run = Network.Wai.Handler.Warp.run 8081 app
+
+
+
