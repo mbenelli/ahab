65ef501ffc2a223e966e40a1617a71ed670e47c3
Marco Benelli <mbenelli@fastmail.com>
2024-03-01 09:27:08 +0100

    Add simplistic calculation of working days

--- lib/Ahab/Time.hs
+++ lib/Ahab/Time.hs
@@ -5,7 +5,7 @@ module Ahab.Time where
 
 import BasicPrelude
 import Data.Text (unpack)
-import Data.Time (NominalDiffTime, UTCTime, diffUTCTime, zonedTimeToUTC)
+import Data.Time (DayOfWeek (Saturday, Sunday), NominalDiffTime, UTCTime, dayOfWeek, diffUTCTime, utctDay, zonedTimeToUTC)
 import Data.Time.Format (defaultTimeLocale, parseTimeM)
 
 data TimeInterval = TimeInterval !UTCTime !UTCTime
@@ -28,3 +28,21 @@ parseTime :: Text -> Maybe UTCTime
 parseTime s = do
   t <- parseTimeM False defaultTimeLocale "%Y-%m-%dT%H:%M:%S%Q%z" $ unpack s
   return $ zonedTimeToUTC t
+
+-- | Return the working days included in the interval.
+--
+-- Weekends are excluded, beginning and end days are included in the count.
+-- Remarks:
+-- Timestamp are in UTC time zone.
+-- Holidays are not considered are they are not known.
+workingDays :: TimeInterval -> Int
+workingDays i =
+  foldl'
+    ( \n x ->
+        if dayOfWeek x == Saturday || dayOfWeek x == Sunday then n else n + 1
+    )
+    0
+    [b .. e]
+  where
+    b = utctDay $ begin i
+    e = utctDay $ end i
